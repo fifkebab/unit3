583f16349dba81a13d227c907b24e644f2e0097e
Fiona Hasib <hasteacher123@gmail.com>
2022-10-13 12:15:58 +0100

    Co-authored-by: Ejaz <ejaz4@users.noreply.github.com>

--- SightseerApp/SettingsScreen.js
+++ SightseerApp/SettingsScreen.js
@@ -1,9 +1,39 @@
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, StyleSheet, Text, Image, TouchableOpacity, View } from 'react-native';
 
 export const SettingsScreen = () => {
     return (
-        <View>
+        <View style={Settings.background}>
+            <View style={Settings.header}>
+                <Text style={Settings.headerText}>Settings</Text>
+            </View>
+
+            <View>
+                <View style={Settings.settingsButton}>
+                    <Image 
+                    source={require("./images/ear.svg")}
+                    />
+                </View>
+
+
+            </View>
 
         </View>
     )
 }
+
+const Settings = StyleSheet.create({
+    background: {
+        backgroundColor: "#F2F1F7",
+        marginLeft: 25
+    },
+    header: {
+        marginTop: 110
+    },
+    headerText: {
+        fontSize: 45,
+        fontWeight: "800"
+    },
+    settingsButton: {
+        backgroundColor: 'white',
+    }
+})
